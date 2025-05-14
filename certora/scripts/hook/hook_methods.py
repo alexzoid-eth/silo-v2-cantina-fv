31706670a0ed921ef9dafdfcdae36f5ccc194cc1
alexzoid <m@alexzoid.com>
2025-05-14 13:08:11 +0400

    remove hook empty spec and confs

--- certora/scripts/hook/hook_methods.py
+++
@@ -1,9 +0,0 @@
-hook_methods_hard = [
-    "liquidationCall_noSToken_noBypass_protectedAllowed(address,uint256)",
-    "liquidationCall_receiveSToken_bypassInterest_protectedAllowed(address,uint256)",
-    "liquidationCall_noSToken_bypassInterest_collateralAllowed(address,uint256)",
-    "liquidationCall_receiveSToken_bypassInterest_collateralAllowed(address,uint256)"
-]
-
-hook_methods_other = [
-]
--- certora/scripts/hook/hook_rule_names.py
+++
@@ -1,7 +0,0 @@
-rule_names = [
-    "hook_test",
-]
-
-rule_names_ex = [
-    "hook_test_inv",
-]
